caac67c2be90f35c0ef58c01c5a2d81a22375114
hsgill3106 <108752326+hsgill3106@users.noreply.github.com>
2022-07-12 10:19:06 -0600

    done

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 //EVENT LISTENERS
 document.getElementById("btn").addEventListener("click", btnClicked);
-
+function btnClicked() {
+  let xA = +document.getElementById("xA").value;
+  let yA = +document.getElementById("yA").value;
+  let xB = +document.getElementById("xB").value;
+  let yB = +document.getElementById("yB").value;
+  let xC = +document.getElementById("xC").value;
+  let yC = +document.getElementById("yC").value;
+  calculate(xA, yA, xB, yB, xC, yC);
+}
 // DEFINING VARIABLES
-let xA = +document.getElementById(xA).value;
-
-let yA = +document.getElementBYId(yA).value;
-
-let xB = +document.getElementById(xB).value;
-
-let yB = +document.getElementById(yB).value;
-
-let xC = +document.getElementById(xC).value;
-
-let yC = +document.getElementById(yC).value;
 
 //DISTANCE CALCULATOR
-function dist(xA, yA, xB, yB) {
-  return Math.sqrt((yB - yA) ** 2 + (xB - xA) ** 2);
+function dist(x1, y1, x2, y2) {
+  return Math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2);
 }
 //MAIN CALCULATOR
-function calculate(AB, BC, AC) {
+function calculate(xA, yA, xB, yB, xC, yC) {
   let AB = +dist(xA, yA, xB, yB);
   let BC = +dist(xB, yB, xC, yC);
   let AC = +dist(xA, yA, xC, yC);
   let perimeter = AB + BC + AC;
-
-  output;
-}
-function btnClicked() {
-  document.getElementById("output").innerHTML = perimeter;
+  let outputP = "The Perimeter is " + perimeter;
+  let outputAB = "AB = " + AB;
+  let outputBC = "BC = " + BC;
+  let outputAC = "AC = " + AC;
+  document.getElementById("outputP").innerHTML = outputP;
+  document.getElementById("outputAB").innerHTML = outputAB;
+  document.getElementById("outputBC").innerHTML = outputBC;
+  document.getElementById("outputAC").innerHTML = outputAC;
 }
